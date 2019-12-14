8233462f962ee915dcd441a67075bb87f203a8a3
CrystalBrooke <55214611+CrystalBrooke@users.noreply.github.com>
2019-12-13 22:26:45 -0500

    Crystal (#4)
    * Hough Circle Detection
    
    Addition of new class that recognizes circles, draws an outline around them, and prints radii of each circle.
    
    * Counting Logic and Resizing
    
    Completed counting logic and resizing methods.
    
    * Final code
    
    Final version of code. Commented and cleaned up.

--- src/CameraSnapshotJavaFX.java
+++ src/CameraSnapshotJavaFX.java
@@ -1,6 +1,6 @@
 //Code taken from https://www.tutorialspoint.com/javaexamples/snapshot_from_system_camera.htm
-//Thanks for the legit code my dude!
 
+//Imports for javafx packages
 import javafx.application.Application;
 import javafx.embed.swing.SwingFXUtils;
 import javafx.scene.Group;
@@ -9,17 +9,22 @@ import javafx.scene.image.ImageView;
 import javafx.scene.image.WritableImage;
 import javafx.stage.Stage;
 
+//imports for opencv pacakges
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.videoio.VideoCapture;
 
+//imports for java awt packages
 import java.awt.image.BufferedImage;
 import java.awt.image.DataBufferByte;
 import java.awt.image.WritableRaster;
+
+//imports for java io packages
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
+//original class for capturing image with webcam/system cam
 public class CameraSnapshotJavaFX extends Application
     {
         Mat matrix = null;
@@ -28,7 +33,7 @@ public class CameraSnapshotJavaFX extends Application
         public void start(Stage stage) throws FileNotFoundException, IOException {
             // Capturing the snapshot from the camera
             CameraSnapshotJavaFX obj = new CameraSnapshotJavaFX();
-            WritableImage writableImage = obj.capureSnapShot();
+            WritableImage writableImage = obj.captureSnapShot();
 
             // Saving the image
             obj.saveImage();
@@ -37,8 +42,8 @@ public class CameraSnapshotJavaFX extends Application
             ImageView imageView = new ImageView(writableImage);
 
             // setting the fit height and width of the image view
-            imageView.setFitHeight(400);
-            imageView.setFitWidth(600);
+            imageView.setFitHeight(4000);
+            imageView.setFitWidth(3000);
 
             // Setting the preserve ratio of the image view
             imageView.setPreserveRatio(true);
@@ -47,7 +52,7 @@ public class CameraSnapshotJavaFX extends Application
             Group root = new Group(imageView);
 
             // Creating a scene object
-            Scene scene = new Scene(root, 600, 400);
+            Scene scene = new Scene(root, 4000, 3000);
 
             // Setting title to the Stage
             stage.setTitle("Capturing an image");
@@ -58,7 +63,7 @@ public class CameraSnapshotJavaFX extends Application
             // Displaying the contents of the stage
             stage.show();
         }
-        public WritableImage capureSnapShot() {
+        public WritableImage captureSnapShot() {
             WritableImage WritableImage = null;
 
             // Loading the OpenCV core library
@@ -91,20 +96,37 @@ public class CameraSnapshotJavaFX extends Application
             }
             return WritableImage;
         }
+        //Saving image to desktop folder
         public void saveImage() {
             // Saving the Image
-            String file = "C:\\Users\\cryst\\Desktop\\Group Project Images\\sanpshot.jpg";
+            String file = "C:\\Users\\cryst\\Desktop\\Group Project Images\\snapshot.jpg";
 
             // Instantiating the imgcodecs class
             Imgcodecs imageCodecs = new Imgcodecs();
 
-            // Saving it again
-            imageCodecs.imwrite(file, matrix);
+            // Saving it again and handling exceptions
+            try {
+                imageCodecs.imwrite(file, matrix);
+            }
+            catch (NullPointerException nullPointerException)
+            {
+                System.out.println("The camera wasn't found!");
+            }
+            catch (Exception exception)
+            {
+                System.out.print("Exception thrown:");
+                System.out.println(exception);
+            }
         }
 
+        //Main
         public static void main(String args[])
         {
-            launch(args);
+
+            //Original method call to launch webcam image capture, now commented out and not in use
+            // launch(args);
+
+            //Method call to run Hough Circle transformation
             new HoughCirclesRun().run(args);
         }
     }
